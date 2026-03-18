0642f76dd8777f3ef49bfb73a559b51b4298be73
walter <waltergusamaya@gmail.com>
2026-03-18 13:52:12 -0300

    fix: path correcto render

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // 🔥 fallback para producción (Render)
-app.get("*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
