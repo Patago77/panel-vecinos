c6919f3d283475ff4363d1ee5f1c52a26ae3957b
walter <waltergusamaya@gmail.com>
2026-03-18 13:09:57 -0300

    fix: ruta principal frontend

--- server.js
+++ server.js
@@ -13,6 +13,9 @@ const db = new Database('panel.db');
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
 
 // ─── CREAR TABLAS ───
 db.exec(`
